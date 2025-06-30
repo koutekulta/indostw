60366bb48b89fe1a0db2fa76cbe866ba9588582b
koutekulta <koutekulta@gmail.com>
2025-06-30 17:33:49 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Indo Stw';
-export const desk = 'Video bokep indo jepang viral ngentot colmek bocil jilbab smp tante.';
+export const desk = 'Video Bokep Indo Jepang Jav Hd Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
 export const url = 'https://indostw.pages.dev';
-export const terbit = '2025-06-27T00:01:03Z';
+export const terbit = '2025-06-26T00:01:03Z';
