5f4b14be8e7d2159f4db6776597f0a273f93ee40
koutekulta <koutekulta@gmail.com>
2025-07-15 00:53:03 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Indo Stw';
-export const desk = 'Video Bokep Indo Jepang Jav Hd Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
-export const url = 'https://indostw.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
